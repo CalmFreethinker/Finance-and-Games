88ebb598ad6bebcf7c0f7ed9c537fa675d2a658a
VRprofile0-0 <61639333+VRprofile0-0@users.noreply.github.com>
2022-10-13 18:35:43 -0400

    Add files via upload

---
+++ pyramid.cpp
@@ -0,0 +1,32 @@
+/*Author: Ricky Vidals
+  CIS 3100 BMWA Spring 2019
+  Assignment 1*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int blank, number, count, i;
+
+	cout << "Enter an integer: ";
+	cin >> number;
+	while (number == 0)
+	{
+		cout << "Error: Enter a value that is greater than 0: ";
+		cin >> number;
+	}
+	for (count = 1, i = 0; count <= number; ++count, i = 0)
+	{
+		for (blank = 1; (number - count) >= blank; ++blank)
+		{
+			cout << "X";
+		}
+		while (i != (2 * count - 1))
+		{
+			cout << "* ";
+			++i;
+		}
+		cout << endl;
+	}
+	return 0;
+}
